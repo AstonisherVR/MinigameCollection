b2e18eb31d1804eba4a716446533690caea6dfdf
Astonisher <astonisherred@gmail.com>
2024-11-22 11:03:55 +0200

    Update GlobalManager.gd

--- Managers/GlobalManager.gd
+++ Managers/GlobalManager.gd
@@ -2,14 +2,8 @@ extends Node
 
 # This takes the current screen size and returns the value as Vector2
 @onready var screen_size = get_viewport().get_visible_rect().end
-#var current_score: int
 
-#func _ready() -> void:
-	#pass
-#
-#func _process(delta: float) -> void:
-	#pass
-
+# Every time an action happends, It checks if its esc and quits if so
 func _input(_event: InputEvent) -> void:
 	if Input.is_action_just_pressed("esc"):
 		get_tree().quit()
